ed71be73aef2273e2531b2b4b4ced8a0ad52ccd1
Alexsandra Mayer <97391174+alexsandramayer@users.noreply.github.com>
2023-08-31 12:15:13 +0500

    Add files via upload

---
+++ homework1/script.js
@@ -0,0 +1,13 @@
+//task 1
+
+/* let degreeCelsius = 25;
+let degreeFahrenheit = (9/5) * degreeCelsius + 32;
+
+console.log(degreeCelsius, degreeFahrenheit); */
+
+//task 2
+
+let myName = 'Alexsandra';
+let admin = myName;
+
+console.log(admin);
